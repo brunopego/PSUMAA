e55a327e6fcde5aa9321fbdd0aba14cc17514122
Bruno Lacerda <brunolpg@outlook.com>
2017-03-16 01:29:59 -0300

    Inicia Projeto

---
+++ cmake-build-debug/CMakeFiles/PSUMAA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSUMAA.dir/main.cpp.o"
+  "PSUMAA.pdb"
+  "PSUMAA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSUMAA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "PSUMAA" << std::endl;
+    return 0;
+}
